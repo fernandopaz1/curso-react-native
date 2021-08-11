f5d38b973db47bfe7b5d94030578af056ffc0731
fernandopaz1 <ferpaz9292@gmail.com>
2021-08-11 11:34:38 -0300

    add: ejemplos de botones

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { divide } from "lodash";
 import React from "react";
-import { Text, View, StyleSheet, Image, Animated,Easing} from "react-native";
+import { Text, View, StyleSheet, Image, Animated,Easing, Button, Alert, TouchableHighlight, TouchableOpacity } from "react-native";
 import image from "./assets/react-logo.png";
 
-spinValue= new Animated.Value(0)
+this._spinValue= new Animated.Value(0)
 Animated.loop(
 // First set up animation 
 Animated.timing(
-    this.spinValue,
+    this._spinValue,
   {
     toValue: 1,
     duration: 3000,
@@ -20,19 +18,38 @@ Animated.timing(
 .start()
 
 // Next, interpolate beginning and end values (in this case 0 and 1)
-const spin = this.spinValue.interpolate({
+const spin = this._spinValue.interpolate({
   inputRange: [0, 1],
   outputRange: ['0deg', '360deg']
 })
 
 const App = () => {
   return (
-    <View style={styles.container}>
+    <View  style={styles.container}>
       <Text style={styles.title}>Hello, world!!</Text>
 			<Animated.Image 
 				source={image}
 				style={styles.image}/>
-	  </View>
+      <Button
+        title="Button"
+        color="#f194ff"
+        onPress={() => Alert.alert('Simple Button pressed')}
+        type="outline"
+      />
+      <TouchableHighlight onPress={() => Alert.alert('Simple Button pressed')}
+        style={styles.button}
+      >
+        <View>
+          <Text style={styles.buttonText}>TouchableHighlight</Text>
+        </View>
+      </TouchableHighlight>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => Alert.alert('Simple Button pressed')}
+      >
+        <Text style={styles.buttonText}>TouchableOpacity</Text>
+      </TouchableOpacity>
+  </View>
   );
 };
 
@@ -40,7 +57,16 @@ const App = () => {
 const styles = StyleSheet.create({
 	container: { flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "rgb(150, 255, 255)"},
 	title: { fontSize: 30 },
-	image: {height: 300, width: 300, transform: [{rotate: spin}]}
+	image: {height: 300, width: 300, transform: [{rotate: spin}]},
+  button: {
+    alignItems: "center",
+    backgroundColor: "#f194ff",
+    padding: 10,
+    borderRadius: 10,
+  },
+  buttonText: { color: "white",
+  fontSize: 20,
+  paddingTop: 2},
 })
 
 export default App;
